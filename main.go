e97f8ca295cf54fe7186e5c69b27be36b1f9344b
pignuante <syaoran215@naver.com>
2020-12-13 23:26:04 +0900

    ,,,

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/pignuante/go-todoList/server"
+	"io/ioutil"
 	"log"
 )
 
+type loginInfo struct {
+	DbAddr string `json:"loginInfo"`
+}
+
 func main() {
+
+	file, err := ioutil.ReadFile("./loginInfo/loginInfo.json")
+	if err != nil {
+		return
+	}
+	var info loginInfo
+	json.Unmarshal(file, &info)
+
 	if err := server.ListenAndServe(server.Config{
 		Address:     ":8080",
-		DatabaseURL: "",
+		DatabaseURL: info.DbAddr,
 	}); err != nil {
 		log.Fatalln(err)
 	}
